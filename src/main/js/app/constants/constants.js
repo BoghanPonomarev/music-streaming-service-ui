338c540b27e4403c72e54beb268ecbef5bec5cda
Bohdan Ponomarev <livenonex@gmail.com>
2020-03-31 18:46:20 +0300

    Add dev constant

--- src/main/js/app/constants/constants.js
+++ src/main/js/app/constants/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_DOMAIN = "http://localhost:8080";
-
+export const SERVER_DOMAIN1 = "http://localhost:8080";
+export const SERVER_DOMAIN = "http://nine-sounds-dev-test2.eu-central-1.elasticbeanstalk.com";
